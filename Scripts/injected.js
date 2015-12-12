b775ef77c6d4d4e4be0fe67e97926fdd634a919f
Dan Loewenherz <dloewenherz@gmail.com>
2015-12-12 14:25:49 -0600

    initial commit

---
+++ Scripts/injected.js
@@ -0,0 +1,31 @@
+var observer = new MutationObserver(function(mutations) {
+    replaceLinks();
+});
+
+function replaceLinks() {
+    var links = document.querySelectorAll(".tweet-text a");
+    for (i in links) {
+        var link = links[i];
+        if (link && link.getAttribute) {
+            var expandedURL = link.getAttribute('data-expanded-url');
+            if (expandedURL) {
+                links[i].href = expandedURL
+            }
+        }
+    }
+}
+
+window.onload = function() {
+    replaceLinks();
+
+    var target = document.querySelector(".stream-items");
+    if (target !== null) {
+        observer.observe(target, {
+            childList: true,
+            subtree: false,
+            attributes: false,
+            characterData: false
+        });
+    }
+}
+
